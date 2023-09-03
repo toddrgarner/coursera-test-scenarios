0b3660762998438b18ad4d7ef793a0ffab5a9481
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2023-09-03 03:09:01 -0700

    modified app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+
+const rangeInput = screen.getByLabelText(/Score:/);
+fireEvent.change(rangeInput, { target: { value: score }});
